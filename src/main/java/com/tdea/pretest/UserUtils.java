ac9c28e5cfb2cae017a5930529b52594392a692a
mateovidales <mateosvidales24@gmail.com>
2022-03-25 19:46:56 -0500

    TestAssertTrue Contraseñas
    TestComparacionContraseñas

--- src/main/java/com/tdea/pretest/UserUtils.java
+++ src/main/java/com/tdea/pretest/UserUtils.java
@@ -2,6 +2,7 @@ package com.tdea.pretest;
 
 public class UserUtils {
     public static boolean isPasswordEqual(String dbPassword, String password) {
+
         return dbPassword.equals(password);
     }
 }
--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,16 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+        boolean resultado = UserUtils.isPasswordEqual("contraseña" , "contraseña");
+           assertTrue(resultado);
+
+
     }
 
 }
